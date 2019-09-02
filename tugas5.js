089cd9a7c47220d3c17de01d086c11b5bfa86aab
noorqidam <noorqidam@gmail.com>
2019-09-02 15:13:28 +0700

    Tugas 5 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas5.js
@@ -0,0 +1,16 @@
+for(var i= 1; i<=20; i++){
+  if(i % 3 === 0){
+  console.log(i)
+  }
+}
+
+// function tugas5() {
+//   var tugas5 = []
+//   for (var i = 1; i <= 20; i++) {
+//     if (i%3 === 0) {
+//       tugas5.push(i)
+//     }
+//   }
+//   return tugas5;
+// }
+// console.log(tugas5());
